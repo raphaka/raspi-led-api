fc714d26ab7f826b255a055bb35e35d6872811d9
Raphael <raphi.kaemper@web.de>
2019-11-08 21:35:33 +0000

    fixed: set_color was not in stream while loop

--- led_api/pin_controller.py
+++ led_api/pin_controller.py
@@ -15,15 +15,17 @@ def start_pigpio():
 #start stream mode on UDP port and change color in realtime
 #TODO Error Handling
 def stream_thread():
+    print('started stream')
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind(("", Glob.config['udp_port']))
     log.info ("Stream mode waiting on port:"+ str(Glob.config['udp_port']))
     while 1:
         str_color, addr = s.recvfrom(1024)
+        str_color = str_color.decode()
         if (str_color == 'exit'):
             return 1
         log.debug("parsing string: " + str(str_color))
-    set_color_by_hex(str_color)
+        set_color_by_hex(str_color)
     return 1
 
 #set gpio values according to rgb-color-hex
