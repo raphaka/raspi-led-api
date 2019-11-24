c980e34bda3a1414aed3c5c4ec04584cef0a970e
Raphael Kaemper <raphi.kaemper@web.de>
2019-11-24 16:36:47 +0100

    config file read from absolute script path instead of CWD

--- led_api/__init__.py
+++ led_api/__init__.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -6,13 +7,14 @@ from flask_marshmallow import Marshmallow
 from led_api.util import Glob,read_json
 from led_api.pin_controller import start_pigpio
 
+api_dir = os.path.dirname(os.path.realpath(__file__))
 app = Flask(__name__)
-app.config.from_pyfile('flask_config.cfg')
+app.config.from_pyfile(api_dir + '/flask_config.cfg')
 db = SQLAlchemy(app)
 ma = Marshmallow(app)   #Wrapper to make SQLalchemy objects JSON Serializable
 from led_api.views import colors,setpins,settings
 
-Glob.config = read_json('config.json')
+Glob.config = read_json(api_dir + '/config.json')
 logging.basicConfig(filename=Glob.config['log_file'],level=logging.DEBUG,
     format='%(asctime)s.%(msecs)03d %(levelname)s: %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
 start_pigpio()
